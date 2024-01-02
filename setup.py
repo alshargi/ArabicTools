177832fff241cf59965e910d6c28a1360ddf6ca6
ALSHARGI <alshargi@hotmail.de>
2024-01-01 23:27:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='FaisalARAtools',
+    name='ArabicTools',
     version='0.0.1',
     author='Dr. Faisal Alshargi',
     author_email='alshargi@hotmail.de',
     description='Testing installation of Package',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/alshargi/FaisalARAtools',
+    url='https://github.com/alshargi/ArabicTools',
     project_urls = {
-        "Bug Tracker": "https://github.com/alshargi/FaisalARAtools/issues"
+        "Bug Tracker": "https://github.com/alshargi/ArabicTools/issues"
     },
     license='MIT',
-    packages=['FaisalARAtools'],
-    package_data={'FaisalARAtools': ['models/*.txt', 'models/*.sav']}, 
+    packages=['ArabicTools'],
     install_requires=['requests'],
 )
