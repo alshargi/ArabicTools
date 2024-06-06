a89a143e7dbdf4c7d49e9bb2b44f766f0e8d5fca
ALSHARGI <alshargi@hotmail.de>
2024-06-06 15:33:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='ArabicTools',
-    version='0.0.2',
+    version='0.0.3',
     author='Dr. Faisal Alshargi',
     author_email='alshargi@hotmail.de',
     description='Testing installation of Package',
