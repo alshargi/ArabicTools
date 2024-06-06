8d337620d0e8587d4567ccaa525f7cc3b1d6ca75
ALSHARGI <alshargi@hotmail.de>
2024-06-06 15:42:04 -0400

    Update __init__.py

--- ArabicTools/__init__.py
+++ ArabicTools/__init__.py
@@ -1,4 +1,4 @@
-from .arabic_functions import  custom_arabic_sentence_splitter, log, SplitBigArabicText, load_txt_file,load_txt_file_ISO, load_txt_file_utf8, save_list_to_file, convert_ara_to_bw, convert_bw_to_ara
+from .arabic_functions import  SplitBigArabicText, log, SplitBigArabicText, load_txt_file,load_txt_file_ISO, load_txt_file_utf8, save_list_to_file, convert_ara_to_bw, convert_bw_to_ara
 from .arabic_statistics import arabic_word_frequency_xml, arabic_punctuation_frequency_xml, log
 
 
